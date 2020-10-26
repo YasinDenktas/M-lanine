c888c6beede69fbb29ec4a5f2af19b5f9464008d
YasinDenktas <YasinDenktas@users.noreply.github.com>
2020-10-26 09:37:12 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+var updateCP = function(elementID, eventType, elementClassName, validationClassName){
+  var selectElement = document.getElementById(elementID);
+  selectElement.addEventListener(eventType, (event) => {
+  var MondialCP = document.getElementsByClassName(elementClassName)[0];
+    var result = selectElement.value;
+  MondialCP.value = result;
+  if(result.length == 5){
+  document.getElementsByClassName(validationClassName)[0].click();
+  }
+  });
+};
